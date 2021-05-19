7756298c69ca79eeef5b73b535f21750ba36dc38
Felipe Martins <tec.felipedsmartins@gmail.com>
2021-05-19 17:04:51 -0300

    change text

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+	<h1>DevSchar</h1>  
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Front end Developer
         </p>
         <a
           className="App-link"
-          href="https://www.google.com/search?q=lagoinha+poa&client=ms-android-samsung&prmd=imsvn&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiFsa_LjNTwAhXCGLkGHRZEDb8Q_AUoAXoECAEQAQ&biw=320&bih=454&dpr=1.5#"
+          href="https://github.com/TecFelipeDsMartins"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         github.com/TecFelipeDsMartins
         </a>
       </header>
     </div>
