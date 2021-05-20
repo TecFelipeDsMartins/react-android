d446d330cb3196cb61be1e1528fed5d08881804c
Felipe D S MArtins <37319545+TecFelipeDsMartins@users.noreply.github.com>
2021-05-20 08:36:41 -0300

    indentation h1 test apenas um teste para pull request

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.jpg';
-import './App.css';
-
+import logo from "./logo.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-	<h1>DevSchar</h1>  
+        <h1>DevSchar</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Front end Developer
-        </p>
+        <p>Front end Developer</p>
         <a
           className="App-link"
           href="https://github.com/TecFelipeDsMartins"
           target="_blank"
           rel="noopener noreferrer"
         >
-         github.com/TecFelipeDsMartins
+          github.com/TecFelipeDsMartins
         </a>
       </header>
     </div>
